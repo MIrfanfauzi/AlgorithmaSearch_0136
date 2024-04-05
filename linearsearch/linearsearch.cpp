ffcd95f4ae154fa28f727dbd7dd050e494fae0b2
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-04-05 10:30:17 +0700

    menambahkan keterangan

--- linearsearch/linearsearch.cpp
+++ linearsearch/linearsearch.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
-int n;
-int i;
+int arr[20]; // Array to be searched
+int n; // Number of elements in the array
+int i; // Index of array element 
 
 void input()
 {
@@ -17,6 +17,7 @@ void input()
             cout << "\nArray shloud have minimum 1 and maximum 20 elements.\n\n";
     }
 
+    //Accept array elements
     cout << "\n---------------------\n";
     cout << " Enter array elements \n";
     cout << "\n---------------------\n";
@@ -30,28 +31,29 @@ void input()
 void LinearSearch()
 {
     char ch;
-    int ctr;
+    int ctr; // number of comparisons
     int item;
 
     do
     {
-        cout << "\nEnter the element you want to search: ";
+        //accept the number to be searched
+        cout << "\nEnter the element you want to search: "; // step 1
         cin >> item;
 
         ctr = 0;
-        i = 0;
-        while (i < n)
+        i = 0; // step 2
+        while (i < n) // step 3
         {
             ctr++;
             if (arr[i] == item)
             {
-                cout << "\n" << item << "found at position " << (i + 1) << endl;
+                cout << "\n" << item << "found at position " << (i + 1) << endl; // step 6
                 break;
             }
-            i++;
+            i++; // step 4
 
         }
-        if (i == 0)
+        if (i == 0) // step 5
             cout << "\n" << item << " not found in the array\n";
         cout << "\nNumber of comparaions: " << ctr << endl;
 
